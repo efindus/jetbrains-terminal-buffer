51cd6ae20edb02f9a8428e1d10fd01fccc7c6f30
efindus <48939809+efindus@users.noreply.github.com>
2026-03-17 00:10:57 +0100

    Implement TerminalBuffer skeleton
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- src/main/java/com/efindus/TerminalBuffer.java
+++ src/main/java/com/efindus/TerminalBuffer.java
@@ -1,4 +1,45 @@
 package com.efindus;
 
+import com.efindus.api.CellStyle;
+
+import java.util.*;
+
+class TerminalRow {
+    char[] chars;
+    CellStyle[] styles;
+
+    TerminalRow(int width) {
+        chars = new char[width];
+        styles = new CellStyle[width];
+
+        for  (int i = 0; i < width; i++) {
+            chars[i] = '\0';
+            styles[i] = new CellStyle();
+        }
+    }
+}
+
 public class TerminalBuffer {
+    private int screenTop;
+    private final TerminalRow[] screen;
+
+    int scrollbackTop;
+    int scrollbackSize;
+    private final TerminalRow[] scrollback;
+
+    private CellStyle currentStyle;
+
+    public TerminalBuffer(int width, int height, int scrollbackLines) {
+        screen = new TerminalRow[height];
+        screenTop = 0;
+
+        for (int i = 0; i < height; i++)
+            screen[i] = new TerminalRow(width);
+
+        scrollback = new TerminalRow[scrollbackLines];
+        scrollbackTop = 0;
+        scrollbackSize = 0;
+
+        currentStyle = new CellStyle();
+    }
 }
