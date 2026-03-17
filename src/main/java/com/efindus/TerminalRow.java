6e2b6dd3617df5c640f851ca57cd740c21198e99
efindus <48939809+efindus@users.noreply.github.com>
2026-03-17 23:59:34 +0100

    Implement writing to TerminalRow
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- src/main/java/com/efindus/TerminalRow.java
+++ src/main/java/com/efindus/TerminalRow.java
@@ -15,4 +15,41 @@ class TerminalRow {
             styles[i] = new CellStyle();
         }
     }
+
+    /**
+     * @param content buffer to write from
+     * @param start starting position from the buffer
+     * @param writeAt position to start writing at
+     * @param ref style to write the text in, makes a copy of the style before writing
+     * @return index of first element not written into the row
+     */
+    int write(char[] content, int start, int writeAt, CellStyle ref) {
+        CellStyle style = new CellStyle(ref);
+        int index = start;
+        for (int i = writeAt; i < chars.length && index < content.length; i++) {
+            chars[i] = content[index++];
+            styles[i] = style;
+        }
+
+        return index;
+    }
+
+    /**
+     * @param c character to write
+     * @param count number of characters to write
+     * @param writeAt position to start writing at
+     * @param ref style to write the text in, makes a copy of the style before writing
+     * @return number of characters written
+     */
+    int write(char c, int count, int writeAt, CellStyle ref) {
+        CellStyle style = new CellStyle(ref);
+        int index = 0;
+        for (int i = writeAt; i < chars.length; i++) {
+            chars[i] = c;
+            styles[i] = style;
+            index++;
+        }
+
+        return index;
+    }
 }
