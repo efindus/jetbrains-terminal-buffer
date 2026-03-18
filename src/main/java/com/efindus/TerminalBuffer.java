ec537e78159b3b9e701ce657fece2a9decd5e4ba
efindus <48939809+efindus@users.noreply.github.com>
2026-03-18 23:50:57 +0100

    Fix errorsin TerminalBuffer found during creating tests
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- src/main/java/com/efindus/TerminalBuffer.java
+++ src/main/java/com/efindus/TerminalBuffer.java
@@ -30,6 +30,7 @@ public class TerminalBuffer {
     }
 
     public TerminalBuffer write(String content) {
+        if (content == null) throw new IllegalArgumentException("content must not be null");
         return write(content.toCharArray());
     }
 
@@ -49,6 +50,7 @@ public class TerminalBuffer {
     }
 
     public TerminalBuffer insert(String content) {
+        if (content == null) throw new IllegalArgumentException("content must not be null");
         return insert(content.toCharArray());
     }
 
@@ -61,6 +63,10 @@ public class TerminalBuffer {
         for (char c : content)
             writeBuffer.insert(new InsertData(c, cStyleCopy));
 
+        // cursor rollback state
+        int cellsWritten = 0;
+        int cX = -1, cY = -1, cScrollSize = 0;
+
         while (writeBuffer.size() > 0) {
             var x = writeBuffer.poll();
 
@@ -70,6 +76,21 @@ public class TerminalBuffer {
 
             r.write(x.c(), getX(), x.s());
             advance();
+
+            cellsWritten++;
+            if (cellsWritten == content.length) {
+                cX = getX();
+                cY = getY();
+                cScrollSize = getScrollbackSize();
+            }
+        }
+
+        // roll back cursor to the first cell after original content, if that ended up being off-screen then to 0, 0
+        int scrollbackDelta = getScrollbackSize() - cScrollSize;
+        if (scrollbackDelta > cY) {
+            setXY(0, 0);
+        } else {
+            setXY(cX, cY - scrollbackDelta);
         }
 
         return this;
@@ -114,17 +135,21 @@ public class TerminalBuffer {
         if (y >= 0)
             return screen.get(y).getCharAt(x);
         else
-            return scrollback.get(scrollback.size() - y).getCharAt(x);
+            return scrollback.get(scrollback.size() + y).getCharAt(x);
     }
 
     public CellStyle getStyleAt(int x, int y) {
         if (x < 0 || x >= screenWidth || y < -scrollback.size() || y >= screenHeight)
             throw new IndexOutOfBoundsException("x=" + x + " y=" + y);
 
+        CellStyle s;
         if (y >= 0)
-            return new CellStyle(screen.get(y).getStyleAt(x));
+            s = screen.get(y).getStyleAt(x);
         else
-            return new CellStyle(scrollback.get(scrollback.size() + y).getStyleAt(x));
+            s = scrollback.get(scrollback.size() + y).getStyleAt(x);
+
+        if (s == null) return null;
+        return new CellStyle(s);
     }
 
     public String getLineAsString(int y) {
